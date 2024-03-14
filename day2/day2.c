f4029da80837f17be8df1530841564de8f3c0193
Giorgius Aurelius <dmgiorgius@gmail.com>
2024-03-14 23:06:48 +0100

    Modified the original solution to account for the second request of the puzzle.

--- day2/day2.c
+++ day2/day2.c
@@ -7,28 +7,54 @@
 
 //========== FUNCTIONS DEFINITIONS ==========//
 
-unsigned int calculate_box_tot_area(unsigned int l, unsigned int w, unsigned int h) {
-
-  unsigned int curr_box_area;
-  unsigned int dim[3] = {l, w, h};
-
-  // Sort array to find the smallest face in terms of area
+void ascendant_sorter(unsigned int *array, int array_length) {
+  /*
+    Function that given an array of uint, sorts its values in ascending order
+  */
   int temp = 0;
-  int array_length = sizeof(dim)/sizeof(dim[0]);
+
   for (int i = 0; i <= array_length; i++) {
     for (int j = i + 1; j < array_length; j++) {
-      if (dim[i] > dim[j]) {
-        temp = dim[i];
-        dim[i] = dim[j];
-        dim[j] = temp;
+      if (array[i] > array[j]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
       }
     }
   }
+}
 
-  // Calulate the area of the box and add the slack
-  curr_box_area = 2*l*w + 2*l*h + 2*w*h + dim[0]*dim[1];
+unsigned int calculate_box_tot_paper(unsigned int dim[]) {
+  /*
+    Calculate current box's needed paper 
+  */
+
+  unsigned int curr_box_paper;
+  unsigned int tot_surface;
+  unsigned int smallest_surface;
+
+  tot_surface = 2*dim[0]*dim[1] + 2*dim[0]*dim[2] + 2*dim[1]*dim[2];
+  smallest_surface = dim[0]*dim[1];
   
-  return curr_box_area;
+  curr_box_paper = tot_surface + smallest_surface;
+
+  return curr_box_paper;
+}
+
+unsigned int calculate_box_tot_ribbon(unsigned int dim[]) {
+  /*
+    Calculate current box's needed ribbon
+  */
+  unsigned int curr_box_ribbon;
+  unsigned int smallest_perimeter;
+  unsigned int volume;
+
+  smallest_perimeter = 2*dim[0]+2*dim[1];
+  volume = dim[0]*dim[1]*dim[2];
+
+  curr_box_ribbon = smallest_perimeter + volume;
+
+  return curr_box_ribbon; 
 }
 
 void main(){
@@ -38,10 +64,10 @@ void main(){
   char input_path[] = "/home/diego/CProjects/advent-of-code-2015/day2/input.txt";
 
   char box_dim[MAX_LINE_LENGTH];
-  unsigned int length = 0;
-  unsigned int width = 0;
-  unsigned int height = 0;
+  unsigned int lwh[3]; 
   unsigned int tot_paper = 0;
+  unsigned int tot_ribbon = 0;
+  int lwh_length;
 
   // Open input file
   if ((input = fopen(input_path, "r")) == NULL) {
@@ -51,11 +77,15 @@ void main(){
 
   // Read input file
   while (fgets(box_dim, MAX_LINE_LENGTH, input) != NULL) {
-    sscanf(box_dim, "%dx%dx%d", &length, &width, &height);
-    tot_paper += calculate_box_tot_area(length, width, height);
+    sscanf(box_dim, "%dx%dx%d", &lwh[0], &lwh[1], &lwh[2]);
+    lwh_length = sizeof(lwh)/sizeof(lwh[0]);
+    ascendant_sorter(lwh, lwh_length);
+    tot_paper += calculate_box_tot_paper(lwh);
+    tot_ribbon += calculate_box_tot_ribbon(lwh);
   }
 
-  printf("Given the box sizes of every present, the elves need: %d squared feet of paper.\n", tot_paper);
+  printf("Given the box sizes of every present, the elves need: %d" 
+  "squared feet of paper and %d feet of ribbon.\n", tot_paper, tot_ribbon);
   fclose(input);
 
   return;
